79cb2db1f893af5d6ab780591b0d57d25c37d27b
GreaterToad <48751000+GreaterToad@users.noreply.github.com>
2019-03-20 11:49:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,36 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+let Prefix = '!'
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
+client.on('message',message => {
+    console.log(message.content)
+    if(message.content.startsWith(`${Prefix}togulag`)) {
+        let role = message.guild.roles.get("557619422915985408");
+        let member = message.mentions.members.first();
+        
+        if(member.roles.has(role.id)) {
+            message.channel.send(`${member.displayName} is already in gulag`)
+        }
+        else{
+            member.addRole(role)
+            message.channel.send(`${member.displayName} has been sent to gulag`)
+        }
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
-  	}
-});
+    }
+    else{
+        if(message.content.startsWith(`${Prefix}release`)){
+            let role = message.guild.roles.get("557619422915985408");
+            let member = message.mentions.members.first();
+            if(member.roles.has(role.id)) {
+                member.removeRole(role)
+                message.channel.send(`${member.displayName} has been released from gulag`)
+            } 
+            else {
+                message.channel.send(`${member.displayName} isnt in gulag`)
+            }
+        }
+    }
 
-client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
-  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
